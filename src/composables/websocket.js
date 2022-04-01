d47416cfa575e007ad779822c8496f22091cd42e
Seyit <seyitbu1111@gmail.com>
2022-04-01 15:39:42 +0500

    Initial commit

---
+++ src/composables/websocket.js
@@ -0,0 +1,3 @@
+export function useWebSocket() {
+  const ws = new WebSocket("ws://localhost:8080");
+}
---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full flex items-center justify-center">
+    <span>WebRTC example project</span>
+    <button class="bg-green p-6" @click="sayHello">
+      Say Hello To Everyone
+    </button>
+  </div>
+</template>
+
+<script setup>
+function sayHello() {}
+</script>
