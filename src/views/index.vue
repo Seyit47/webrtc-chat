4ea1d92b44ff84dcd4050dcae5b630674d1c16fa
Seyit <seyitbu1111@gmail.com>
2022-04-06 12:47:06 +0500

    feat: room video chat implemented

--- src/views/index.vue
+++ src/views/index.vue
@@ -7,19 +7,9 @@
     <button @click="startCall">Start Local Stream</button>
     <div class="flex w-full items-center justify-center">
       <span>Local</span>
-      <video
-        ref="localVideo"
-        class="w-full h-full"
-        autoplay
-        playsinline
-      ></video>
+      <video ref="localVideo" class="w-1/2 h-1/2" autoplay playsinline></video>
       <span>Remote</span>
-      <video
-        ref="remoteVideo"
-        class="w-full h-full"
-        autoplay
-        playsinline
-      ></video>
+      <div ref="videoEls"></div>
     </div>
   </div>
 </template>
@@ -27,54 +17,140 @@
 <script setup>
 import { useRTCPeerConnection } from "@/composables/rtcpeerconnection.js";
 import { useWebSocket } from "@/composables/websocket.js";
-import { onMounted, ref } from "vue";
+import { computed } from "@vue/reactivity";
+import { nextTick, onMounted, ref, watch } from "vue";
 
 const { ws, sendData } = useWebSocket();
-const { pc } = useRTCPeerConnection();
 
 const localVideo = ref();
-const remoteVideo = ref();
+const remoteVideos = ref([]);
 
-let localStream = null;
-let remoteStream = null;
+const videoEls = ref();
+
+const clients = ref([]);
+
+const you = ref();
+
+const pcs = ref([]);
+
+const localStream = async () => {
+  return await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+};
 
 const msgs = ref([]);
 
 ws.value.onmessage = function (data) {
   const payload = JSON.parse(data.data);
 
-  console.log(payload);
-
   if (payload.type === "message") {
     msgs.value.push(payload.data);
   }
   if (payload.type === "offer") {
-    onOffer(payload.offer);
+    onOffer(payload.peer, payload.offer);
   }
   if (payload.type === "answer") {
-    onAnswer(payload.answer);
+    onAnswer(payload.peer, payload.answer);
   }
   if (payload.type === "candidate") {
-    onCandidate(payload.candidate);
+    onCandidate(payload.peer, payload.candidate);
+  }
+  if (payload.type === "metadata") {
+    you.value = payload.client;
+  }
+  if (payload.type === "new-client") {
+    let pushed = false;
+    if (clients.value.length === 0) {
+      clients.value.push(payload.client);
+      setPcs(payload.client);
+      pushed = true;
+    }
+    if (!checkIfExists(clients.value, payload.client) && !pushed) {
+      clients.value.push(payload.client);
+      setPcs(payload.client);
+    }
+  }
+  if (payload.type === "client-disconnected") {
+    const index = clients.value.reduce((acc, curValue, curIndex) => {
+      if (curValue.id === payload.client.id) {
+        return curIndex;
+      }
+      return acc;
+    }, -1);
+    if (index >= 0) {
+      clients.value.splice(index, 1);
+      removePcs(payload.client);
+    }
   }
 };
 
-pc.value.onicecandidate = function (event) {
-  if (event.candidate) {
-    sendData({
-      type: "candidate",
-      candidate: event.candidate,
-    });
-  }
-};
-remoteStream = new MediaStream();
+function checkIfExists(arr, client) {
+  return arr.reduce((acc, curValue) => {
+    if (curValue.id === client.id) {
+      return true;
+    }
+    return acc;
+  }, false);
+}
 
-pc.value.ontrack = function (event) {
-  event.streams[0].getTracks().forEach((track) => {
-    remoteStream.addTrack(track);
+function setPcs(client) {
+  const { pc } = useRTCPeerConnection();
+
+  pcs.value.push({
+    client,
+    pc: pc.value,
   });
-  remoteVideo.value.srcObject = remoteStream;
-};
+
+  addLocalStreamToAllPeerConnections(pc.value);
+
+  pc.value.onicecandidate = function (event) {
+    if (event.candidate) {
+      sendData({
+        type: "candidate",
+        candidate: event.candidate,
+        peer: you.value,
+        to: client,
+      });
+    }
+  };
+
+  let tracked = false;
+
+  pc.value.ontrack = function (event) {
+    if (!tracked) {
+      const stream = new MediaStream();
+      if (event.streams[0]) {
+        event.streams[0].getTracks().forEach((track) => {
+          stream.addTrack(track);
+        });
+      }
+      const el = document.createElement("video");
+      el.classList.add(`index-${client.id}`);
+      el.style.width = "100%";
+      el.style.height = "100%";
+      el.playsInline = true;
+      el.autoplay = true;
+      el.srcObject = stream;
+      videoEls.value.appendChild(el);
+      tracked = true;
+    }
+  };
+}
+
+function removePcs(client) {
+  const index = pcs.value.reduce((acc, curValue, curIndex) => {
+    if (curValue.client.id === client.id) {
+      return curIndex;
+    }
+    return acc;
+  }, -1);
+  if (index >= 0) {
+    pcs.value[index].pc.close();
+    pcs.value.splice(index, 1);
+  }
+}
 
 function sayHello() {
   const payload = {
@@ -84,58 +160,91 @@ function sayHello() {
   sendData(payload);
 }
 
-function startCall() {
-  createOffer();
+async function startCall() {
+  for (let i = 0; i < pcs.value.length; i++) {
+    await addLocalStreamToAllPeerConnections(pcs.value[i].pc);
+    await createOffer(pcs.value[i]);
+  }
+}
+
+async function addLocalStreamToAllPeerConnections(pc) {
+  const stream = await localStream();
+  stream.getTracks().forEach((track) => {
+    pc.addTrack(track, stream);
+  });
 }
 
 async function startLocalStream() {
-  localStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
-
-  localStream.getTracks().forEach((track) => {
-    pc.value.addTrack(track, localStream);
-  });
-
-  localVideo.value.srcObject = localStream;
+  localVideo.value.srcObject = await localStream();
 }
 
-async function createOffer() {
-  const offer = await pc.value.createOffer();
+async function createOffer(data) {
+  const offer = await data.pc.createOffer();
   const payload = {
     type: "offer",
     offer,
+    peer: you.value,
+    to: data.client,
   };
-  console.log(payload);
   sendData(payload);
 
-  pc.value.setLocalDescription(offer);
+  data.pc.setLocalDescription(offer);
 }
 
-async function onOffer(offer) {
-  pc.value.setRemoteDescription(new RTCSessionDescription(offer));
+async function onOffer(client, offer) {
+  try {
+    const index = getPC(pcs.value, client);
+    let pc = index >= 0 ? pcs.value[index].pc : null;
+    pc.setRemoteDescription(new RTCSessionDescription(offer));
 
-  const answer = await pc.value.createAnswer();
-  pc.value.setLocalDescription(answer);
+    const answer = await pc.createAnswer();
+    pc.setLocalDescription(answer);
 
-  const payload = {
-    type: "answer",
-    answer,
-  };
+    const payload = {
+      type: "answer",
+      answer,
+      peer: you.value,
+      to: client,
+    };
 
-  sendData(payload);
+    sendData(payload);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function onAnswer(answer) {
-  pc.value.setRemoteDescription(new RTCSessionDescription(answer));
+function onAnswer(client, answer) {
+  try {
+    const index = getPC(pcs.value, client);
+    let pc = index >= 0 ? pcs.value[index].pc : null;
+    pc.setRemoteDescription(new RTCSessionDescription(answer));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function onCandidate(candidate) {
-  pc.value.addIceCandidate(new RTCIceCandidate(candidate));
+function onCandidate(client, candidate) {
+  try {
+    const index = getPC(pcs.value, client);
+    let pc = index >= 0 ? pcs.value[index].pc : null;
+    pc.addIceCandidate(new RTCIceCandidate(candidate));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function getPC(arr, client) {
+  return arr.reduce((acc, curValue, curIndex) => {
+    if (curValue.client.id === client.id) {
+      return curIndex;
+    }
+    return acc;
+  }, -1);
 }
 
 onMounted(() => {
-  startLocalStream();
+  startLocalStream().then(() => {
+    startCall();
+  });
 });
 </script>
